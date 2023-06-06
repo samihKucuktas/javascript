d1970943647242c08d8121b433c514f00aa22ef3
owen haes <owen.haes@student.odisee.be>
2023-06-06 14:56:43 +0200

    timer gefixed css mobile is in orde css desktop is nog fucked

--- timer.js
+++ timer.js
@@ -1,26 +1,27 @@
 let time = 0; //minutes * 60 seconds
 let givealert = true;
-let refreshIntervalId = setInterval(updateCountdown, 1000); //update every 1 second
-
+setInterval(updateCountdown, 1000); //update every 1 second
+let minutes = 0
+let seconds = 0
 function updateCountdown() {
-    if (checkWin() !== true){
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
+    if (!checkWin()){
+        minutes = Math.floor(time / 60);
+        seconds = time % 60;
 
         seconds = seconds < 10 ? '0' + seconds : seconds;
         const contdownEl = document.getElementById("countdown");
         contdownEl.innerHTML = `${minutes}:${seconds}`;
-
+console.log(time)
         time++;
-    }else if (givealert === true){
+    }else if (givealert){
         time--;
         if(time<60){
-            alert("YOU WIN! \n all nuts have been deposited \n you took " + time + " seconds");
             givealert = false;
-        }else if(time>60){
-            seconds--;
+            alert("YOU WIN! \n all nuts have been deposited \n you took " + seconds + " seconds");
+            givealert = false;
+        }else{
+            givealert = false;
             alert("YOU WIN! \n all nuts have been deposited \n you took " + minutes + " minute and " + seconds + " seconds");
-            givealert = false;
         }
     }
 }
